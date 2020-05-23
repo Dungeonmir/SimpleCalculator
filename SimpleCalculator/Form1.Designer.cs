644a2854cebff718d0ceb34f7c885e0112cc9f9b
Dungeonmir <63905738+Dungeonmir@users.noreply.github.com>
2020-05-23 18:22:19 +0300

    Simple Calculator

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SimpleCalculator
+{
+    partial class SimpleCalculator
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFirstNumber = new System.Windows.Forms.Label();
+            this.lbSecondNumber = new System.Windows.Forms.Label();
+            this.lbAnswer = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbFirstNumber
+            // 
+            this.lbFirstNumber.AutoSize = true;
+            this.lbFirstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbFirstNumber.Location = new System.Drawing.Point(58, 70);
+            this.lbFirstNumber.Name = "lbFirstNumber";
+            this.lbFirstNumber.Size = new System.Drawing.Size(119, 24);
+            this.lbFirstNumber.TabIndex = 0;
+            this.lbFirstNumber.Text = "First Number";
+            // 
+            // lbSecondNumber
+            // 
+            this.lbSecondNumber.AutoSize = true;
+            this.lbSecondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbSecondNumber.Location = new System.Drawing.Point(39, 239);
+            this.lbSecondNumber.Name = "lbSecondNumber";
+            this.lbSecondNumber.Size = new System.Drawing.Size(150, 24);
+            this.lbSecondNumber.TabIndex = 1;
+            this.lbSecondNumber.Text = "Second Number";
+            // 
+            // lbAnswer
+            // 
+            this.lbAnswer.AutoSize = true;
+            this.lbAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbAnswer.Location = new System.Drawing.Point(58, 312);
+            this.lbAnswer.Name = "lbAnswer";
+            this.lbAnswer.Size = new System.Drawing.Size(153, 24);
+            this.lbAnswer.TabIndex = 2;
+            this.lbAnswer.Text = "The answer is: ...";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstNumber.Location = new System.Drawing.Point(195, 66);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(150, 33);
+            this.txtFirstNumber.TabIndex = 3;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSecondNumber.Location = new System.Drawing.Point(195, 235);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(150, 33);
+            this.txtSecondNumber.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(52, 141);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(50, 50);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSubstract.Location = new System.Drawing.Point(133, 141);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(50, 50);
+            this.btnSubstract.TabIndex = 6;
+            this.btnSubstract.Text = "-";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDivide.Location = new System.Drawing.Point(214, 141);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(50, 50);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMultiply.Location = new System.Drawing.Point(295, 141);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(50, 50);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(351, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "CL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SimpleCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 386);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.lbAnswer);
+            this.Controls.Add(this.lbSecondNumber);
+            this.Controls.Add(this.lbFirstNumber);
+            this.Name = "SimpleCalculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbFirstNumber;
+        private System.Windows.Forms.Label lbSecondNumber;
+        private System.Windows.Forms.Label lbAnswer;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class SimpleCalculator : Form
+    {
+        public SimpleCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            float firstNumber;
+            float secondNumber;
+            float result;
+            try
+            {
+                firstNumber = float.Parse(txtFirstNumber.Text);
+                secondNumber = float.Parse(txtSecondNumber.Text);
+                result = firstNumber + secondNumber;
+                lbAnswer.Text = "The answer is: " + result.ToString();
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtFirstNumber.Clear();
+            txtSecondNumber.Clear();
+            lbAnswer.Text = "The answer is: ...";
+        }
+
+        private void btnSubstract_Click(object sender, EventArgs e)
+        {
+            float firstNumber;
+            float secondNumber;
+            float result;
+            try
+            {
+                firstNumber = float.Parse(txtFirstNumber.Text);
+                secondNumber = float.Parse(txtSecondNumber.Text);
+                result = firstNumber - secondNumber;
+                lbAnswer.Text = "The answer is: " + result.ToString();
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            float firstNumber;
+            float secondNumber;
+            float result;
+            try
+            {
+                firstNumber = float.Parse(txtFirstNumber.Text);
+                secondNumber = float.Parse(txtSecondNumber.Text);
+                result = firstNumber / secondNumber;
+                lbAnswer.Text = "The answer is: " + result.ToString();
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            float firstNumber;
+            float secondNumber;
+            float result;
+            try
+            {
+                firstNumber = float.Parse(txtFirstNumber.Text);
+                secondNumber = float.Parse(txtSecondNumber.Text);
+                result = firstNumber * secondNumber;
+                lbAnswer.Text = "The answer is: " + result.ToString();
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
